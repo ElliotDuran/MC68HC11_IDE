3d4cc2a9b11070ef0b197523a044d17f6ab27470
Elliot Duran Macedo <elliotd70@gmail.com>
2020-09-20 17:17:28 -0500

    Function get_code was created

--- interface.py
+++ interface.py
@@ -1,13 +1,12 @@
 # !/usr/bin/env python
 
 
-from tkinter import Tk, Menu
+from tkinter import Tk, Menu, messagebox, filedialog
 
 
 root = Tk()
 root.title('IDE MC68HC11')
 root.iconbitmap('app_icon.ico')
-# root.attributes('-fullscreen', True)
 
 menu_bar = Menu(root)
 
@@ -45,24 +44,12 @@ edit_menu.add_separator()
 edit_menu.add_command(label="Select All",
                       accelerator='Ctrl+A')
 
-run_menu = Menu(menu_bar, tearoff=0)
-run_menu.add_command(label="Compile",
-                     accelerator='F5')
-run_menu.add_separator()
-run_menu.add_checkbutton(label="Make lst",
-                         onvalue=0, offvalue=1)
-run_menu.add_checkbutton(label="Make hex",
-                         onvalue=0, offvalue=1)
-run_menu.add_checkbutton(label="Make HTML",
-                         onvalue=0, offvalue=1)
-
 help_menu = Menu(menu_bar, tearoff=0)
 help_menu.add_command(label="About")
 help_menu.add_command(label="Help")
 
 menu_bar.add_cascade(label="File", menu=file_menu)
 menu_bar.add_cascade(label="Edit", menu=edit_menu)
-menu_bar.add_cascade(label="Run", menu=run_menu)
 menu_bar.add_cascade(label="About", menu=help_menu)
 root.config(menu=menu_bar)
 
